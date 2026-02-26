c820515fd9e5e8f42147ac85bca11545f9d10d39
Marco Aguilera <marcoaguilera2005.maac@gmail.com>
2026-02-26 01:06:11 -0600

    AvanceClase

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
+#pip install pyqt6 pyqt6-tools
+#.\venv\Scripts\Activate.ps1 (activar el entorno virtual)
 from PyQt6 import QtWidgets, uic
 import sys
+from controllers.login_controller import LoginController
 
 class Login(QtWidgets.QMainWindow):
-    __init__(self):
-    super().__init__()
-    uic.loadUi("./views/login.ui", self)
-
-    class (QtWidgets.QMainWindow):
-    __init__(self):
-    super().__init__()
-    uic.loadUi("./views/main.ui" self)
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("./views/login.ui", self)
+        self.controller = LoginController(self, self)
+class Sell(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("./views/main.ui", self)
 
 class AppManager:
     def __init__(self):
         self.login_window = Login()
-        self.sell_window = sell()
+        self.sell_window = Sell()
         #muestra la pantalla
         self.login_window.show()
 
 app = QtWidgets.QApplication(sys.argv)
 manager = AppManager()
-    sys.exit(app.exec())
+sys.exit(app.exec())
